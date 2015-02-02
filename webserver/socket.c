a0672ce7de0aa64e88ce2db6024a43a113a94416
Loic Delwaulle <delwaulle.loic@hotmail.fr>
2015-02-02 14:47:16 +0100

    verification entete http

--- webserver/socket.c
+++ webserver/socket.c
@@ -50,6 +50,19 @@ int afficherMessage(int socket_client){
 	return 0;
 }
 
+char *substr(char *src,int pos,int len) { 
+	char *dest=NULL;                        
+	if (len>0) {                  
+   		/* allocation et mise à zéro */          
+   		dest = calloc(len+1, 1);      
+   		/* vérification de la réussite de l'allocation*/  
+    		if(NULL != dest) {
+       			strncat(dest,src+pos,len);            
+    		}
+  	}                                       
+	return dest;                            
+}
+
 int traitement_entete_http(char ligne []){
 	int nbMots=1;
 	int i;
@@ -61,12 +74,30 @@ int traitement_entete_http(char ligne []){
 	for(i=0;i<strlen(ligne);i++){
 		if(ligne[i]==' '){
 			nbMots++;
+			if(nbMots==3){
+				char *http;
+				char *version;
+				http=substr(ligne,i+1,4);
+				//printf("%s\n",http);
+				if(strcmp(http,"HTTP")!=0)
+					return -1;
+				version=substr(ligne,i+6,strlen(ligne));
+				//printf("%s\n",version);
+				char c=version[0];
+				char c2=version[2];
+				int v1=(int)c-48;
+				int vv=(int)c2-48;
+				if(v1!=1 || (vv!=0 && vv!=1)) {
+					return -1;
+				}
+			}
 		}
 	}
 	if(nbMots!=3)
 		return -1;
-	return 0;
 
+	
+	return 0;
 }
 
 #define BUFF_SIZE 128
@@ -78,18 +109,22 @@ void traiterClient(int socket_client){
 	while(fgets(p,BUFF_SIZE,f)!=NULL){
 		if(i==0){
 			if(traitement_entete_http(p)!=0){
-				printf("BAD REQUEST");
+				fprintf(f, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: %i\r\n400 Bad request\r\n",17);
 				exit(0);
 			}	
-			else
-				afficherMessage(socket_client);
 		}
-		printf("<Websys> %s",p);
+		if(p[0]=='\n' || (strcmp(p,"\r\n")==0))
+			break;
+		
+		printf("<websys> %s", p);
 		i++;
 	}
+
 	/*while((i=read(socket_client,p,BUFF_SIZE))> 0){
 		write(socket_client, p, i);
 	}*/
+	afficherMessage(socket_client);
+	fprintf(f,"HTTP/1.1 200 OK\r\nContent-Length: %i\r\n",10);
 }
 
 int attendre_socket(int socket_serveur){
